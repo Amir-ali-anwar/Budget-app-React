20aca9f3434eee4e539fc68f3db1f4e4dbe80102
Amir Ali anwar <amiralianwar611@gmail.com>
2023-03-13 21:48:39 +0500

    setting up the project routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,26 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Dashboard from './components/Dashboard'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Dashboard />,
+    // loader: rootLoader,
+    // children: [
+    //   {
+    //     path: "/",
+    //     element: <Dashboard />,
+    //     loader: teamLoader,
+    //   },
+    // ],
+  },
+]);
+
+
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    <RouterProvider router={router} />
+  </div>;
 }
 
 export default App;
